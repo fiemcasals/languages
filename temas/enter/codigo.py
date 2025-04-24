d87e5d7511ba9bc5681e85f52d37ffabfb9e002d
fiemcasals <101604436+fiemcasals@users.noreply.github.com>
2025-04-24 15:59:53 +0000

    enter

---
+++ temas/enter/codigo.py
@@ -0,0 +1,17 @@
+print("a")
+print("b")
+
+print("a", end="")
+print("b")
+
+print("a", end="-")
+print("b")
+
+print("a \nb")
+
+nombre ="mauri"
+ocupacion = "profesor"
+print(f"\n{nombre} es: {ocupacion}")
+
+print("con un espacio en el medio")
+print(f"\n{nombre} es:\n{ocupacion}")
